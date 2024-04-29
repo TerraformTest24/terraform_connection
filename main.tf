caefa58eca394371abdb3cdbcbe62dd9397c0243
Miguel Currea M <79986499+mcurream@users.noreply.github.com>
2024-04-29 00:01:50 -0500

    New file from Terraform
    Full instructions for Terraform CLI

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = "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"
+  owner = "TerraformTest24"
+}
+
+resource "github_membership" "membership_for_user_x" {
+  username = "mcurream"
+  role     = "admin"
+}
+
+resource "github_repository" "terraform_connection" {
+	name = "terraform_connection"
+	description = "Terraform CLI connection and creation test through IaC"
+	visibility = "public"
+}
+
+resource "github_branch" "development" {
+  repository = "terraform_connection"
+  branch     = "development"
+}
+
+resource "github_branch" "feature" {
+  repository = "terraform_connection"
+  branch     = "feature"
+}
+
+resource "github_branch" "hotfix" {
+  repository = "terraform_connection"
+  branch     = "hotfix"
+}
+
+
